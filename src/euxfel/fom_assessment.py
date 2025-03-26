db23c4caad66ae36c67f1a72d17786c3a5c3cc71
Simon Billinge <simon.billinge@gmail.com>
2025-03-26 14:25:04 -0400

    merge then black

--- src/euxfel/fom_assessment.py
+++ src/euxfel/fom_assessment.py
@@ -8,7 +8,7 @@ from euxfel.functions import build_delay_dict, find_nearest, set_limits
 from euxfel.parsers import get_args, preprocessing_args
 from euxfel.plotters import assessment_plotter
 
-matplotlib.use('TkAgg')
+matplotlib.use("TkAgg")
 
 # run_number = 204
 # sample_name = 'Bismuth'
@@ -42,24 +42,29 @@ def main():
     rel_path_to_data = Path(args.path_to_data)
     input_path = cwd / rel_path_to_data
     str_run_number = str(args.run_number).zfill(4)
-    on_data_path = input_path/f'run{str_run_number}_delay_intensity_on.npy'
-    off_data_path = input_path/f'run{str_run_number}_delay_intensity_off.npy'
-    q_path = input_path/f'run{str_run_number}_q_values.npy'
-    delay_positions_path = input_path/f'run{str_run_number}_delay_positions.npy'
-    metadata.update({'cwd': cwd})
+    on_data_path = input_path / f"run{str_run_number}_delay_intensity_on.npy"
+    off_data_path = input_path / f"run{str_run_number}_delay_intensity_off.npy"
+    q_path = input_path / f"run{str_run_number}_q_values.npy"
+    delay_positions_path = (
+        input_path / f"run{str_run_number}_delay_positions.npy"
+    )
+    metadata.update({"cwd": cwd})
 
     on = np.load(on_data_path)
     off = np.load(off_data_path)
     q = np.load(q_path)
     args = set_limits(args, q)
     delay = np.load(delay_positions_path)
-    delay = args.t0-delay     # remove the t0 offset
+    delay = args.t0 - delay  # remove the t0 offset
     if args.normalize_to_target_id is None:
-        midpoint = (max(delay) + min(delay))/2.
+        midpoint = (max(delay) + min(delay)) / 2.0
         args.normalize_to_target_id = find_nearest(delay, midpoint)
     target_key = delay[args.normalize_to_target_id]
-    morph_cfg = morph.morph_default_config(scale=args.initial_scale,stretch=args.initial_stretch,
-                                           smear=args.initial_smear)
+    morph_cfg = morph.morph_default_config(
+        scale=args.initial_scale,
+        stretch=args.initial_stretch,
+        smear=args.initial_smear,
+    )
 
     raw_delays = {}
     for i, step in enumerate(delay):
@@ -70,17 +75,32 @@ def main():
             on[i],
             off[i],
             q_min=args.q_min_assess,
-            q_max=args.q_max_assess
+            q_max=args.q_max_assess,
         )
-
 
     morph_delays = {}
     target = raw_delays[target_key]
     for delay_t, data in raw_delays.items():
-        morphed = morph.morph(data[0], data[1], data[0], target[1], rmin=args.q_min_normalize, rmax=args.q_max_normalize, **morph_cfg) #on
-        _, on_morph, _, _ = morphed['morph_chain'].xyallout
-        morphed = morph.morph(data[0], data[2], data[0], target[1], rmin=args.q_min_normalize, rmax=args.q_max_normalize, **morph_cfg) #off
-        _, off_morph, _, _ = morphed['morph_chain'].xyallout
+        morphed = morph.morph(
+            data[0],
+            data[1],
+            data[0],
+            target[1],
+            rmin=args.q_min_normalize,
+            rmax=args.q_max_normalize,
+            **morph_cfg,
+        )  # on
+        _, on_morph, _, _ = morphed["morph_chain"].xyallout
+        morphed = morph.morph(
+            data[0],
+            data[2],
+            data[0],
+            target[1],
+            rmin=args.q_min_normalize,
+            rmax=args.q_max_normalize,
+            **morph_cfg,
+        )  # off
+        _, off_morph, _, _ = morphed["morph_chain"].xyallout
         morph_delays = build_delay_dict(
             morph_delays,
             delay_t,
@@ -89,16 +109,17 @@ def main():
             off_morph,
             morphed=morphed,
             q_min=args.q_min_assess,
-            q_max=args.q_max_assess
+            q_max=args.q_max_assess,
         )
     # if points_away_t0_plot_on_off is not None:
     #     t0_index = find_nearest(delay,0)
     #     time_away_t0_index_plot = t0_index + points_away_t0_plot_on_off
     #     time_away_t0 = delay[time_away_t0_index_plot]
 
-    #plot_function(raw_delays)
+    # plot_function(raw_delays)
     assessment_plotter(morph_delays, args)
     print(metadata)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/euxfel/plotters.py
+++ src/euxfel/plotters.py
@@ -4,20 +4,21 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
+
 def assessment_plotter(delays, args):
-    fig, (ax0,ax1,ax2,ax3) = plt.subplots(4,1,figsize=(8, 14))
+    fig, (ax0, ax1, ax2, ax3) = plt.subplots(4, 1, figsize=(8, 14))
     keys = [key for key in delays.keys()]
     delay_times_l1 = [delay[4] for delay in delays.values()]
     delay_times_off = [delay[5] for delay in delays.values()]
     delay_times_on = [delay[6] for delay in delays.values()]
     delay_times_l2 = [delay[7] for delay in delays.values()]
     delay_times_rw = [delay[9] for delay in delays.values()]
-    #cmap = cm.get_cmap('viridis', len(keys))
-    cmap = matplotlib.colormaps['viridis']
+    # cmap = cm.get_cmap('viridis', len(keys))
+    cmap = matplotlib.colormaps["viridis"]
     colors = [cmap(i) for i in np.linspace(0, 1, len(keys))]
     key_to_color_idx = {key: i for i, key in enumerate(keys)}
-    #cmap = matplotlib.colormaps.get_cmap('viridis', len(keys))
-    #key_to_color_idx = {key: i for i, key in enumerate(keys)}
+    # cmap = matplotlib.colormaps.get_cmap('viridis', len(keys))
+    # key_to_color_idx = {key: i for i, key in enumerate(keys)}
     max_diff, min_diff = -np.inf, np.inf
     for key, delay in delays.items():
         # if key == time_away_t0:
@@ -26,32 +27,35 @@ def assessment_plotter(delays, args):
         color = colors[key_to_color_idx[key]]
         max_diff = max([max_diff, max(delay[3][:-20])])
         min_diff = min([max_diff, min(delay[3][:-20])])
-        ax0.plot(delay[0],delay[1],label=key,color=color)
-        ax1.plot(delay[0],delay[3],label=key,color=color)
-        ax1.axvline(x=args.q_min_assess,color='red')
-        ax1.axvline(x=args.q_max_assess,color='red')
+        ax0.plot(delay[0], delay[1], label=key, color=color)
+        ax1.plot(delay[0], delay[3], label=key, color=color)
+        ax1.axvline(x=args.q_min_assess, color="red")
+        ax1.axvline(x=args.q_max_assess, color="red")
     # ax1.plot(delay[0],args.on_plot,label='on',color='black')
     # ax1.plot(delay[0],args.off_plot,label='off',color='orange')
-    ax2.plot(keys,delay_times_off,marker='o', linestyle='-',label='off')
-    ax2.plot(keys,delay_times_on,marker='o', linestyle='-',label='on')
-    ax3.plot(keys,np.sqrt(delay_times_l2),marker='o', linestyle='-',label='diff')
+    ax2.plot(keys, delay_times_off, marker="o", linestyle="-", label="off")
+    ax2.plot(keys, delay_times_on, marker="o", linestyle="-", label="on")
+    ax3.plot(
+        keys, np.sqrt(delay_times_l2), marker="o", linestyle="-", label="diff"
+    )
     # ax3.plot(keys,np.sqrt(delay_times_rw),marker='o', linestyle='-',label='diff')
-    ax0.set_xlabel('Q [1/A]')
-    ax0.set_ylabel('Pump On Intensity [a.u.]')
-    ax1.set_xlabel('Q [1/A]')
-    ax1.set_ylim([min_diff,max_diff])
-    ax1.set_ylabel('Pump Off Intensity [a.u.]')
-    ax2.set_xlabel('Q [1/A]')
-    ax2.set_ylabel('On-Off Intensity [a.u.]')
-    ax3.set_xlabel('Time delay (ps)')
-    ax3.set_ylabel('Sum intensities')
+    ax0.set_xlabel("Q [1/A]")
+    ax0.set_ylabel("Pump On Intensity [a.u.]")
+    ax1.set_xlabel("Q [1/A]")
+    ax1.set_ylim([min_diff, max_diff])
+    ax1.set_ylabel("Pump Off Intensity [a.u.]")
+    ax2.set_xlabel("Q [1/A]")
+    ax2.set_ylabel("On-Off Intensity [a.u.]")
+    ax3.set_xlabel("Time delay (ps)")
+    ax3.set_ylabel("Sum intensities")
     # ax4.set_xlabel('Time delay (ps)')
     # ax4.set_ylabel('RMS')
     ax3.legend()
-    ax0.set_title(f'sample = {args.sample_name}, run = {args.run_number}, qmin = {args.q_min_assess:.2f}, qmax = {args.q_max_assess:.2f}')
+    ax0.set_title(
+        f"sample = {args.sample_name}, run = {args.run_number}, qmin = {args.q_min_assess:.2f}, qmax = {args.q_max_assess:.2f}"
+    )
     # ax1.set_title(f'I(q) On vs Off, time_delay ={args.time_away_t0}, run = {args.run_number}')
-    ax2.set_title(f'I(q) On - I(q) Off run = {args.run_number}')
+    ax2.set_title(f"I(q) On - I(q) Off run = {args.run_number}")
     # ax4.set_title(f'Figure of Merit run = {args.run_number}, q_min = {args.q_min_assess:.2f}, q_max = {args.q_max_assess:.2f}')
     plt.tight_layout()
     plt.show()
-
